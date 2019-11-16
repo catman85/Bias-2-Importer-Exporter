7de398bd981178103161a6b32c788c025edbf95e
jimmy <celenh@yahoo.com>
2019-11-16 20:49:36 +0200

    file structure done

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
+    <top-menu></top-menu>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import TopMenu from '@/components/TopMenu.vue'
   export default {
-    name: 'bias-2-ie'
+    name: 'bias-2-ie',
+    components: {
+      TopMenu
+    },
   }
 </script>
 
