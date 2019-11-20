de4b792ea70850c8fffe7bb3c8683cf063727672
jimmy <jim@thinkpad.gr>
2019-11-20 21:16:13 +0200

    Bootstrap Vue Big Fix

--- src/renderer/plugins/bootstrap-vue.js
+++ src/renderer/plugins/bootstrap-vue.js
@@ -1,5 +1,11 @@
 // ATTENTION
 // npm install bootstrap-vue bootstrap-css-only
+// ATTENTION
+// also add in package.json otherwise the npm run build will brake
+// "optionalDependencies": {
+//     "jquery": "1.9.1 - 3"
+//  },
+
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 
