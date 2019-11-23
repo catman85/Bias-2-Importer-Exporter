e1497fdfc067ed9616085178cc3be71b071e84aa
jimmy <jim@thinkpad.gr>
2019-11-23 02:20:20 +0200

    app logo

--- src/main/index.js
+++ src/main/index.js
@@ -25,7 +25,8 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     height: 563,
     useContentSize: true,
-    width: 1000
+    width: 1000,
+    icon: __dirname + '/../../build/icons/icon.ico'
   })
   mainWindow.loadURL(winURL)
 
